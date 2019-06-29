9cf4d2bacc62e4dc48f03fbc3bb36e00943397b0
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-06-29 10:15:29 +0300

    Add Doxygen documentation for CIdentificator class

--- src/cid.cpp
+++ src/cid.cpp
@@ -6,6 +6,11 @@
  *  Description : CIdentificator class implementation.                      *
  ****************************************************************************/
 
+/**
+ * @file cid.cpp
+ * CIdentificator class implementation.
+ */
+
 #include <string.h>
 
 #include "cid.h"
@@ -13,19 +18,64 @@
 unsigned long CIdentificator::ulClassID = 101;
 char CIdentificator::strClassName[] = "CIdentificator";
 
+/** @brief Default constructor. Does nothing */
 CIdentificator::CIdentificator() {}
 
+/**
+ * @brief Retrieves class identification name
+ *
+ * Retrieves class identification name (ClassName).
+ * @remarks For the identification the static member #strClassName is used.
+ * Thus member's value is the same for all objects of type CIdentificator and
+ * it descendants. If you want to identify a descendant of CIdentificator
+ * differently you must redeclare this member and initialize it like you want.
+ * @param ulStrSz
+ * @param strName
+ */
 void CIdentificator::GetClassName(unsigned long ulStrSz, char *strName) const {
     strncpy(strName, strClassName, ulStrSz);
 }
 
+/**
+ * @brief Checks if a class can identify itself with a specified number
+ *
+ * This function can be used to check if a class or it ancestors can identify
+ * themselves with the specified number. Thus you can check if the object
+ * you use is from the type you want.
+ * @remarks It is generally a good idea to override this function in your
+ * descendants when you redeclare the class identification members ClassID.
+ * A common way to implement this function is to compare the ID of your class
+ * with the ID passed as parameter to the function and in case they don't
+ * match to recurse invoking the base class function. Recursion will end in
+ * base class CIdentificator::IsA function where only compare is done. All
+ * classes from the module hierarchy have this behavior.
+ * @param ulCompareID ID to be compared
+ * @return True when match otherwise false
+ */
 bool CIdentificator::IsA(unsigned long ulCompareID) const {
     return ( ulCompareID == ulClassID );
 }
 
+/**
+ * @brief Checks if a class can identify itself with a specified name
+ *
+ * This function can be used to check if a class or it ancestors can identify
+ * themselves with the specified name. Thus you can check if the object
+ * you use is from the type you want.
+ * @remarks It is generally a good idea to override this function in your
+ * descendants when you redeclare the class identification member ClassName.
+ * A common way to implement this function is to compare the Name of your class
+ * with the Name passed as parameter to the function and in case they don't
+ * match to recurse invoking the base class function. Recursion will end in
+ * base class CIdentificator::IsA function where only compare is done. All
+ * classes from the module hierarchy have this behavior.
+ * @param strCompareName Name to be compared
+ * @return True when match otherwise false
+ */
 bool CIdentificator::IsA(const char *strCompareName) const {
     return ( strcmp(strCompareName, strClassName) == 0 );
 }
 
+/** @brief Destructor. Does nothing */
 CIdentificator::~CIdentificator() {}
 
--- src/cid.h
+++ src/cid.h
@@ -6,6 +6,11 @@
  *  Description : CIdentificator class declaration.                         *
  ****************************************************************************/
 
+/**
+ * @file cid.h
+ * CIdentificator class declaration.
+ */
+
 #ifndef CIDENTIFICATOR_H
 #define CIDENTIFICATOR_H 1
 
@@ -15,30 +20,48 @@
 #error "error: File cid.h requires c++ compiler!"
 #endif
 
-/* Class  : CIdentificator
- * Purpose: Type identification.
+/**
+ * @brief Base class for identification
+ *
+ * This class is used as a base for all other classes in the project. It
+ * provides general identification mechanism for his descendants. A
+ * descendant of CIdentificator can identify itself with a single unsigned
+ * decimal number or with a name.
+ *
+ * @remark This class doesn't support copy constructor and assignment
+ * operator. They are declared in the private section but are not defined.
+ * Check class members to see more information about inheriting this class.
  */
 class DNETMOD_API CIdentificator {
 private:
-    /* Copy constructor and assignment operator not supported for this class */
     CIdentificator(const CIdentificator &);
     CIdentificator & operator = (CIdentificator &);
 protected:
+    /** Class's identifier */
     static unsigned long ulClassID;
+    /** CLass's name */
     static char strClassName[];
 public:
-    /* constructors */
     CIdentificator();
-    /* get/set */
     unsigned long GetClassID(void) const;
     void GetClassName(unsigned long ulStrSz, char *strName) const;
-    /* identification */
+
     virtual bool IsA(unsigned long ulCompareID) const;
     virtual bool IsA(const char *strCompareName) const;
-    /* destructor */
+
     virtual ~CIdentificator();
 };
 
+/**
+ * @brief Retrieves class identification number
+ *
+ * Retrieves class identification number (ClassID).
+ * @remarks For the identification the static member #ulClassID is used.
+ * Thus member's value is the same for all objects of type CIdentificator and
+ * it descendants. If you want to identify a descendant of CIdentificator
+ * differently you must redeclare this member and initialize it like you want.
+ * @return Class identification number
+ */
 inline unsigned long CIdentificator::GetClassID(void) const {
     return ulClassID;
 }
