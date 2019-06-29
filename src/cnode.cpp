0a45cd9a5dab02bc5a61314e32c752ea451e443a
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-06-29 10:15:08 +0300

    Add Doxygen documentation for CNode class

--- src/cnode.cpp
+++ src/cnode.cpp
@@ -6,6 +6,11 @@
  *  Description : CNode class implementation.                               *
  ****************************************************************************/
 
+/**
+ * @file cnode.cpp
+ * CNode class implementation.
+ */
+
 #include <string.h>
 
 #include "cnode.h"
@@ -13,6 +18,11 @@
 unsigned long CNode::ulClassID = 201;
 char CNode::strClassName[] = "CNode";
 
+/**
+ * @brief Default constructor
+ *
+ * Initializes all members accordingly.
+ */
 CNode::CNode() : CIdentificator() {
     bActive = false;
     ucMacID = 0;
@@ -20,6 +30,14 @@ CNode::CNode() : CIdentificator() {
     ucProducedConnSize = 0;
 }
 
+/**
+ * @brief Constructor with parameters
+ *
+ * Initializes members with provided values.
+ * @param ucMID MAC ID of the node.
+ * @param ucCCS Consumed connection size of the node.
+ * @param ucPCS Produced connection size of the node.
+ */
 CNode::CNode(unsigned char ucMID, unsigned char ucCCS, unsigned char ucPCS) {
     bActive = false;
     ucMacID = 0;
@@ -28,26 +46,64 @@ CNode::CNode(unsigned char ucMID, unsigned char ucCCS, unsigned char ucPCS) {
     ucProducedConnSize = ucPCS;
 }
 
+/**
+ * @brief Set MAC ID of the node
+ *
+ * Use this functions to set MAC ID of the node.
+ * @remark The function will set the new value only if node is not active.
+ * @param ucMID MAC ID of the node.
+ */
 void CNode::SetMacID(unsigned char ucMID) {
     if ( !bActive && ucMID < DEVICENET_MAX_DEVICES )
         ucMacID = ucMID;
 }
 
+/**
+ * @brief Sets count of consumed bytes by the node from the network
+ *
+ * Use this functions to set the count of bytes consumed by the node from
+ * the network.
+ * @param ucCCS Consumed data bytes from the network.
+ */
 void CNode::SetConsumedConnSize(unsigned char ucCCS) {
     ucConsumedConnSize = ucCCS;
 }
 
+/**
+ * @brief Set count of node produced bytes to the network
+ *
+ * Use this functions to set count of produced bytes by the node to
+ * the network.
+ * @param ucPCS Node produced data bytes to the network.
+ */
 void CNode::SetProducedConnSize(unsigned char ucPCS) {
     ucProducedConnSize = ucPCS;
 }
 
+/**
+ * Checks if class can identify itself with the specified number. If not
+ * then passes the check to the base class.
+ * @param ulCompareID ID to be compared.
+ * @return True when match otherwise false.
+ */
 bool CNode::IsA(unsigned long ulCompareID) const {
     return (ulClassID == ulCompareID) ? true : CIdentificator::IsA(ulCompareID);
 }
 
+/**
+ * Checks if class can identify itself with the specified name. If not
+ * then passes the check to the base class.
+ * @param strCompareName Name to be compared.
+ * @return True when match otherwise false.
+ */
 bool CNode::IsA(const char *strCompareName) const {
     return (!strcmp(strClassName, strCompareName)) ? true : CIdentificator::IsA(strCompareName);
 }
 
+/**
+ * @brief Destructor
+ *
+ * Does nothing.
+ */
 CNode::~CNode() {}
 
--- src/cnode.h
+++ src/cnode.h
@@ -6,6 +6,11 @@
  *  Description : CNode class declaration.                                  *
  ****************************************************************************/
 
+/**
+ * @file cnode.h
+ * CNode class declaration.
+ */
+
 #ifndef CNODE_H
 #define CNODE_H 1
 
@@ -17,21 +22,34 @@
 
 #include "cid.h"
 
-/* Class  : CNode
- * Purpose: Representing a node in DeviceNet network.
+/**
+ * @brief Represents a node in DeviceNet™ network
+ *
+ * This class is provided for generalization of nodes in a DeviceNet™ network.
+ * It is a base class for CInterface and CDevice classes representing
+ * respectively interface (master device) and slave device in the network.
+ * Each of these has a MAC ID, a consumed and produced connection size on
+ * the network and a state which to tell you if the node is active on
+ * the network or not.
+ * @remarks Copy constructor and assignment operator not supported for this class.
  */
 class DNETMOD_API CNode : public CIdentificator {
 private:
-    /* Copy constructor and assignment operator not supported for this class */
     CNode(const CNode&);
     CNode& operator =(const CNode&);
 protected:
+    /** Class's ID */
     static unsigned long ulClassID;
+    /** Class's name */
     static char strClassName[];
 protected:
+    /** Flag showing whether node is active or not. */
     bool bActive;
+    /** MAC identifier of the node */
     unsigned char ucMacID;
+    /** Consumed connection size */
     unsigned char ucConsumedConnSize;
+    /** Produced connection size */
     unsigned char ucProducedConnSize;
 public:
     /* constructors */
@@ -52,18 +70,44 @@ public:
     virtual ~CNode();
 };
 
+/**
+ * @brief Checks node activity on the network
+ *
+ * Use this function to find if node is active on the network or not.
+ * @return True if active, false if not.
+ */
 inline bool CNode::IsActive(void) const {
     return bActive;
 }
 
+/**
+ * @brief Retrieves MAC ID of the node
+ *
+ * Use this functions to retrieve MAC ID of the node.
+ * @return MAC ID of the node.
+ */
 inline unsigned char CNode::GetMacID(void) const {
     return ucMacID;
 }
 
+/**
+ * @brief Retrieves count of consumed bytes by the node from the network
+ *
+ * Use this functions to get the count of bytes consumed by the node from
+ * the network.
+ * @return Consumed data bytes from the network.
+ */
 inline unsigned char CNode::GetConsumedConnSize(void) const {
     return ucConsumedConnSize;
 }
 
+/**
+ * @brief Retrieves count of node produced bytes to the network
+ *
+ * Use this functions to get the count of produced bytes by the node to
+ * the network.
+ * @return Node produced data bytes to the network.
+ */
 inline unsigned char CNode::GetProducedConnSize(void) const {
     return ucProducedConnSize;
 }
